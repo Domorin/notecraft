dda77022ce2095b995c5ac9d74933438f056453a
Cody <domorin821@gmail.com>
2023-07-25 11:59:45 -0700

    improve responsiveness

--- src/pages/react/components/main.tsx
+++ src/pages/react/components/main.tsx
@@ -6,11 +6,11 @@ export default function MainPage() {
 	return (
 		<div className="flex h-screen items-center justify-center bg-base-300">
 			<ThemePicker />
-			<div className="rounded-box flex h-3/4 w-3/4 overflow-hidden border-2 border-neutral shadow-lg">
-				<div className="rounded-l-box w-1/6 bg-base-200">
+			<div className="rounded-box m-12 flex h-3/4 overflow-hidden border-2 border-neutral shadow-lg">
+				<div className="rounded-l-box w-[12rem] bg-base-200">
 					<Sidebar />
 				</div>
-				<div className="rounded-r-box w-5/6 flex-grow-0 bg-base-100 p-8">
+				<div className="rounded-r-box w-[64rem] flex-grow-0 bg-base-100 p-8">
 					<LoadableNote />
 				</div>
 			</div>
