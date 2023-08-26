7cf7c15c9e40ad05132264c1f66b758620687fe6
Cody <domorin821@gmail.com>
2023-08-25 18:19:15 -0700

    improve build script

--- scripts/production/build_docker.sh
+++ scripts/production/build_docker.sh
@@ -1,7 +1,7 @@
 # TODO: change to docker-compose.prod.yml
 echo "Building docker images"
 docker compose -f docker-compose.yml -f docker-compose.build.yml --progress=plain build ws app
-echo "Removing tmp"
+# Make sure tmp is empty before we start
 rm -rf tmp
 echo "Creating tmp"
 mkdir -p tmp/images
@@ -10,6 +10,16 @@ echo "Saving images..."
 docker save notecraft-ws notecraft-app -o tmp/images.tar
 echo "Done saving images"
 echo "Sending images to production server"
-rsync -v -r -P package.json docker-compose.yml docker-compose.prod.yml .env tmp note-smith-production:/home/domorin/notecraft_test/
+# Copy .env.prod to .env for production (and delete it after)
+cp .env.prod .env
+rsync -v -r -P x.json docker-compose.yml docker-compose.prod.yml .env tmp note-smith-production:/home/domorin/notecraft/
 echo "Done sending images to production server"
-ssh note-smith-production "cd /home/domorin/notecraft_test && docker load -i tmp/images.tar && npm run prodResourcesUp"
+rm .env
+rm -rf tmp
+
+# Stop app and ws containers and remove their images
+ssh note-smith-production "docker rm $(docker stop $(docker ps -a -q --filter ancestor=notecraft-app --format="{{.ID}}")) && docker rmi notecraft-app"
+ssh note-smith-production "docker rm $(docker stop $(docker ps -a -q --filter ancestor=notecraft-ws --format="{{.ID}}")) && docker rmi notecraft-ws"
+# Load images and start containers
+ssh note-smith-production "cd /home/domorin/notecraft && docker load -i tmp/images.tar && npm run prodResourcesUp"
+echo "Successfully deployed"
