3b5893083888893a4f1fa493ebd00a8698ea1405
Cody <domorin821@gmail.com>
2023-09-13 23:30:29 -0700

    prefetch login info + welcome message; improve build script

--- notecraft-app/src/pages/index.tsx
+++ notecraft-app/src/pages/index.tsx
@@ -1,5 +1,10 @@
+import { withSessionSsr } from "@/lib/session";
 import MainPageContainer from "@/react/components/main_page_container";
 import WelcomeNote from "@/react/components/note/welcome_note";
+import { appRouter } from "@/server/trpc/routers/_app";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { GetServerSidePropsContext } from "next";
+import superjson from "superjson";
 
 export default function IndexPage() {
 	return (
@@ -8,3 +13,37 @@ export default function IndexPage() {
 		</MainPageContainer>
 	);
 }
+
+export const getServerSideProps = withSessionSsr(
+	async function getServerSideProps(context: GetServerSidePropsContext) {
+		const helpers = createServerSideHelpers({
+			router: appRouter,
+			ctx: {
+				api: {
+					req: context.req,
+					res: context.res,
+				},
+			},
+			transformer: superjson,
+		});
+
+		// TODO: prefetch login
+
+		// Server side prefetch only note's content
+		// We can prefetch other things as well, but content is most important and we do not want to increase time to first byte
+		await Promise.all([
+			helpers.user.info.prefetch(),
+			helpers.note.htmlContent.prefetch({
+				slug: "welcome" as string,
+			}),
+			helpers.note.metadata.prefetch({
+				slug: "welcome" as string,
+			}),
+		]);
+		return {
+			props: {
+				trpcState: helpers.dehydrate(),
+			},
+		};
+	}
+);
--- scripts/production/build_docker.sh
+++ scripts/production/build_docker.sh
@@ -3,7 +3,7 @@ docker compose -f docker-compose.yml -f docker-compose.build.yml --progress=plai
 # Make sure tmp is empty before we start
 rm -rf tmp
 echo "Creating tmp"
-mkdir -p tmp/images
+mkdir -p tmp
 echo "Saving images..."
 # User docker/save load to send it to prod server? https://stackoverflow.com/questions/23935141/how-to-copy-docker-images-from-one-host-to-another-without-using-a-repository
 docker save notecraft-ws notecraft-app -o tmp/images.tar
@@ -11,11 +11,8 @@ echo "Done saving images"
 echo "Sending images to production server"
 rsync -v -r -P package.json docker-compose.yml docker-compose.prod.yml .env.production tmp note-smith-production:/home/domorin/notecraft/
 echo "Done sending images to production server"
-rm -rf tmp
+# rm -rf tmp
 
-# Stop app and ws containers and remove their images
-ssh note-smith-production "docker rm $(docker stop $(docker ps -a -q --filter ancestor=notecraft-app --format="{{.ID}}")) && docker rmi notecraft-app"
-ssh note-smith-production "docker rm $(docker stop $(docker ps -a -q --filter ancestor=notecraft-ws --format="{{.ID}}")) && docker rmi notecraft-ws"
-# Load images and start containers
-ssh note-smith-production "cd /home/domorin/notecraft && docker load -i tmp/images.tar && npm run prodResourcesUp"
+# Delete previous images, load new images and start containers
+ssh note-smith-production "cd /home/domorin/notecraft && npm run prodCleanResources; docker load -i tmp/images.tar && npm run prodResourcesUp"
 echo "Successfully deployed"
