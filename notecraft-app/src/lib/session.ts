b6942f32ad05d7db7411414ba92bb0ef625b9816
Cody <domorin821@gmail.com>
2023-09-14 21:16:57 -0700

    trying samesite lax

--- notecraft-app/src/lib/session.ts
+++ notecraft-app/src/lib/session.ts
@@ -30,7 +30,7 @@ export const ironOptions: IronSessionOptions = {
 	// secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
 	cookieOptions: {
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
+		sameSite: "lax",
 	},
 };
 
